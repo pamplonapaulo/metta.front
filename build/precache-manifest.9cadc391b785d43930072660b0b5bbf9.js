3d97823924ada489c1e055d780d247c3882f5674
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-05-11 18:40:21 -0300

    first commit

---
+++ build/precache-manifest.9cadc391b785d43930072660b0b5bbf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b490f216c9f85dfbb37e973c19b15aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2044fe03c25a2d43607f",
+    "url": "/static/css/main.0a1041b4.chunk.css"
+  },
+  {
+    "revision": "3373dfe271bc95b581ff",
+    "url": "/static/js/2.a6c6f2c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6c6f2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2044fe03c25a2d43607f",
+    "url": "/static/js/main.2f3e4f6b.chunk.js"
+  },
+  {
+    "revision": "73f94224884e0ba06838",
+    "url": "/static/js/runtime-main.df8613b9.js"
+  },
+  {
+    "revision": "2e028b11db074ba0afec7a59c90b6339",
+    "url": "/static/media/logo.2e028b11.svg"
+  }
+]);
